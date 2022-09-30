f15c5be1d03b0fff09927c749e207a58f0f6b577
Alex <98185072+Alex13070@users.noreply.github.com>
2022-09-30 16:30:09 +0200

    Terminado y comentado

--- horario/index.php
+++ horario/index.php
@@ -2,7 +2,7 @@
 
     const HORAS = [
         ["Horas","16:00 <br> 16:55", "16:55 <br> 16:55", "17:50 <br> 16:55", "17:50 <br> 18:45", "19:10 <br> 20:05","20:05 <br> 21:00", "21:00 <br> 21:55"],
-        ["Lunes", "DWEC", "DWEC", "DWEC","", "DWEC", "EIE", "DWEC"], 
+        ["Lunes", "DWEC", "DWEC", "DWEC","", "EIE", "EIE", "INGLES"], 
         ["Martes", "INGLES", "DAW", "DAW","", "DIW", "DIW", "DIW"],
         ["Miercoles", "DIW", "DIW", "DIW","", "DWES","DWES","DWES"],
         ["Jueves", "EIE","DAW","DAW","","DWES","DWES","DWES"],
@@ -18,6 +18,9 @@
         "Viernes" => ["DWES","DWES","DWES","", "DWEC","DWEC","DWEC"]
     ];
 
+    /**
+     * Esta funcion lo que hace es contar el numero de clases siguientes que son iguales tras la clase introducida. 
+     */
     function cuantosIgualesDespues($dia, $hora): int {
         $nombre = HORAS[$dia][$hora];
         $cont = 0;
@@ -28,31 +31,28 @@
             }
         }
         */
-        $empiezo = false;
-        $sigo = true;
-        if($hora < 5) {
-            for ($i=0; $i < 5 && $sigo; $i++) { 
-                if (HORAS[$dia][$i] == $nombre) {
-                    $empiezo = true;
-                    $cont++;
-                }
-                else {
-                    if ($empiezo) {
-                        $sigo = false;
-                    }
-                }
-            }
+
+        $empiezo = false; // Para controlar cuando comienza a contar las clases.
+        $sigo = true; // Ve si tiene que seguir contando.
+
+        
+        if($hora < 5) { // Aqui contamos las que son antes del recreo.
+            $inicio = 0;
+            $fin = 5;
         }
-        else {
-            for ($i=5; $i < count(HORAS[$dia]) && $sigo; $i++) { 
-                if (HORAS[$dia][$i] == $nombre) {
-                    $empiezo = true;
-                    $cont++;
-                }
-                else {
-                    if ($empiezo) {
-                        $sigo = false;
-                    }
+        else { // Aqui contamos las que son despues del recreo.
+            $inicio = 5;
+            $fin = count(HORAS[$dia]);
+        }
+
+        for ($i= $inicio; $i < $fin && $sigo; $i++) { 
+            if (HORAS[$dia][$i] == $nombre) {
+                $empiezo = true; //Comienza a contrar las clases.
+                $cont++;
+            }
+            else {
+                if ($empiezo) {
+                    $sigo = false; // Si la que sigue no es igual, deja de contar.
                 }
             }
         }
@@ -60,6 +60,9 @@
         return $cont;
     }
 
+    /**
+     * Aqui vemos si la posicion anterior es igual a la siguiente.
+     */
     function igualAlAnterior($dia, $hora): bool{
     
         $nombre = HORAS[$dia][$hora];
@@ -72,9 +75,11 @@
         return $esIgual;
     }
 
+    /**
+     * Pone estilos a las celdas
+     */
     function pintarColor($nombre) {
         $array = ["DWEC", "EIE", "INGLES", "DAW", "DIW", "DWES"];
-
         return (in_array($nombre, $array))?$nombre:"a";
     }
     
@@ -87,10 +92,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--
+    <!-- -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    -->    
+    <!-- -->    
     <title>Arrays</title>
     <style>
         table {
@@ -134,10 +139,6 @@
         .DWES {
             background-color: lightsalmon;
         }
-
-        body {
-            background-image: url(https://c.tenor.com/lOVqHrlKsT0AAAAC/obama-prism.gif);
-        }
     </style>
 </head>
 <body>
@@ -146,6 +147,7 @@
         <div class="col-md-3"></div>
         <div class="col-md-6 d-flex justify-content-center">
 
+            <!-- Recorriendo el primer array -->
             <div class="tabla">
                 <table>
                     <tr><td colspan="<?= count(HORAS)?>" style="text-align: center;">Horario</td></tr>
@@ -163,7 +165,7 @@
             </div>
 
 
-
+            <!-- Recorriendo el array_map -->
             <div class="tabla">
                 <table>
                     <tr>
@@ -189,6 +191,7 @@
                 </table>
             </div> 
 
+            <!-- Recorriendo el array teniendo en cuenta la cantidad de horas. -->
             <div class="tabla">
                 <table>
                     <tr><td colspan="<?= count(HORAS)?>" style="text-align: center;">Horario</td></tr>
@@ -196,7 +199,9 @@
                     <?php for ($i=0; $i < count(HORAS[0]); $i++) { ?>        
                         <tr>
                             <?php for ($j=0; $j < count(HORAS); $j++) { ?>
-                                <?php if (!igualAlAnterior($j, $i)) { ?>
+                                <?php if (!igualAlAnterior($j, $i)) { // Si es igual al anterior, no tenemos que pintarlo, pues esta tendra rowspan ?>
+                                    
+                                    <!-- Mira cuantas horas mas tenemos con la misma asignatura -->
                                     <td  rowspan="<?= cuantosIgualesDespues($j, $i) ?>" class="<?= pintarColor(HORAS[$j][$i])?>">
                                         <?php echo HORAS[$j][$i];?> 
                                     </td> 
