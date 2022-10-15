0ad0a4e027f7a5078076fad9a9aca797cc8af3e2
Alex <98185072+Alex13070@users.noreply.github.com>
2022-10-16 00:21:34 +0200

    Arreglado el 11

--- ut3/codigo-embedido/11.php
+++ ut3/codigo-embedido/11.php
@@ -6,11 +6,58 @@
 // Si es una cadena cambiará las mayúsuclas por minúsculas y viceversa.
 // En caso de no estar entre estos valores lo dejará sin modificar.
 
-function mierda() {
-    
+function cambiarLetras(string $var) {
+    $retorno = "";
+
+    for ($i=0; $i < strlen($var); $i++) { 
+        if ($var[$i] == strtoupper($var[$i])) {
+            $retorno .= strtolower($var[$i]);
+        }
+        else {
+            $retorno .= strtoupper($var[$i]);
+        }
+
+        echo $retorno."<br>";
+    }
+
+
+
+    return $retorno;
+
 }
 
+function cosasRaras(array &$array) {
+    $numEntero = 2;
 
+    foreach($array as &$value) {
+        if (gettype($value) == "integer") {
+            $value = pow($value, $numEntero++);            
+        }
+        else {
+            $numEntero = 2;
+
+            if (gettype($value) == "double") {
+                $value = - $value;
+            }
+            else if(gettype($value) == "string") {
+                $value = cambiarLetras($value);
+
+            }
+
+        }
+
+    }
+
+}
+
+$array = [3, 4, "h", 3, 'hola', [1,2,3], [1], "h"];
+
+cosasRaras($array);
+
+
+print_r($array);
+
+//cambiarLetras("Hola me LLamo Pepe")
 
 
 ?>
